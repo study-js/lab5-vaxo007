5d12a9979a1d138b4558dc603742ecadf1f35915
Your Name <you@example.com>
2017-12-06 18:56:56 +0400

    l502

---
+++ L502/script.js
@@ -0,0 +1,4 @@
+var i=document.getElementById("users");
+var d=document.getElementById("empty-list");
+alert("users is empty:"+(i.children.length==0));
+alert("empty-list is empty:"+(d.children.length==0));
